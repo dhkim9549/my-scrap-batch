6a74bf1f9bb9dfa953dd95c140c36de22ce352b2
root <dhkim95@hotmail.com>
2025-10-22 06:09:41 +0000

    lint

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import { tabletojson } from "tabletojson";
 
-let apiUrl = "https://hf.go.kr/ko/sub02/sub02_01_07_01.do";
-// let apiUrl = "https://api.scraperapi.com/?api_key=a6feef9544ee61c2d5b078836ca638f4&url=https%3A%2F%2Ffinance.naver.com%2Fsise%2Fsise_market_sum.naver";
+const apiUrl = "https://hf.go.kr/ko/sub02/sub02_01_07_01.do";
 
 async function main() {
   let table = "";
-  await tabletojson.convertUrl(apiUrl, function (tablesAsJson) {
+  await tabletojson.convertUrl(apiUrl, (tablesAsJson) => {
     console.log(
       "tablesAsJson => " + Object.prototype.toString.call(tablesAsJson),
     );
@@ -17,9 +16,7 @@ async function main() {
   console.log("table => " + Object.prototype.toString.call(table));
   console.log(table);
 
-  let table2 = table.filter(function (e) {
-    return e.금융기관.startsWith("하나");
-  });
+  const table2 = table.filter((e) => e.금융기관.startsWith("하나"));
 
   console.log("table2 => " + Object.prototype.toString.call(table2));
   console.log(table2);
